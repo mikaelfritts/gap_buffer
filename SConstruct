a5ee2ffbe6f73e599a548c4971f51c06052e82f9
Will Jones <drjones78@gmail.com>
2011-12-09 17:25:24 -0500

    Updated SConstruct - compiles the gap_buffer as a shared library

--- SConstruct
+++ SConstruct
@@ -2,7 +2,16 @@ import os
 
 VariantDir('build','src', duplicate=0)
 
+
+env = Environment()
+env.SharedLibrary('lib/gap_buffer', ['src/lib/gap_buffer.c'])
+
+
 env = Environment(LIBS = 'gap_buffer', LIBPATH='lib')
 
-env.Library('lib/gap_buffer', ['build/lib/gap_buffer.c'])
+# force binary to look in the local lib folder for libgap_buffer.so at runtime
+
+env.Append( LINKFLAGS = Split('-z origin') )
+env.Append( RPATH = env.Literal(os.path.join('\\$$ORIGIN', os.pardir, 'lib')))
 env.Program('bin/gap_buffer_demo','build/gap_buffer_demo.c')
+
--- src/gap_buffer_demo.c
+++ src/gap_buffer_demo.c
@@ -20,62 +20,43 @@ int main()
 
     gb = gap_buffer_new();
 
-    gap_buffer_put_str(gb, "helwd");        gap_buffer_print(gb);
-
-    gap_buffer_move_cursor(gb, -1);         //gap_buffer_print(gb);
-
-    gap_buffer_put_str(gb, "orl");          gap_buffer_print(gb);
-
-    gap_buffer_move_cursor(gb, -7);         //gap_buffer_print(gb);
+    gap_buffer_put_str(gb, "helwd");            gap_buffer_print(gb);
+    gap_buffer_move_cursor(gb, -1);
+    gap_buffer_put_str(gb, "orl");              gap_buffer_print(gb);
+    gap_buffer_move_cursor(gb, -7);
 
     // this does the same thing as gap_buffer_put while in replace mode
-    gap_buffer_replace(gb, 'H');            gap_buffer_print(gb);
+    gap_buffer_replace(gb, 'H');                gap_buffer_print(gb);
+    gap_buffer_move_cursor(gb, 2);
+    gap_buffer_put_str(gb, "lo!#o ");           gap_buffer_print(gb);
+    gap_buffer_delete(gb);                      gap_buffer_print(gb);
+    gap_buffer_delete(gb);                      gap_buffer_print(gb);
+    gap_buffer_delete(gb);                      gap_buffer_print(gb);
+    gap_buffer_delete(gb);                      gap_buffer_print(gb);
+    gap_buffer_put(gb, ' ');                    gap_buffer_print(gb);
 
-    gap_buffer_move_cursor(gb, 2);          //gap_buffer_print(gb);
-
-    gap_buffer_put_str(gb, "lo!#o ");       gap_buffer_print(gb);
-
-    gap_buffer_delete(gb);                  gap_buffer_print(gb);
-
-    gap_buffer_delete(gb);                  gap_buffer_print(gb);
-
-    gap_buffer_delete(gb);                  gap_buffer_print(gb);
-
-    gap_buffer_delete(gb);                  gap_buffer_print(gb);
-
-    gap_buffer_put(gb, ' ');                gap_buffer_print(gb);
-
+    /* demonstrate replace mode */
     gap_buffer_set_mode(gb, REPLACE_MODE);
 
     // these replace instead of insert now!
-    //gap_buffer_replace(gb, 'W');            gap_buffer_print(gb);
-    gap_buffer_put(gb, 'W');            gap_buffer_print(gb);
+    gap_buffer_put(gb, 'W');                    gap_buffer_print(gb);
 
-    // now puts are back to insert mode!
+    /* now puts are back to insert mode! */
     gap_buffer_set_mode(gb, INSERT_MODE);
 
-    gap_buffer_move_cursor(gb, 3);          //gap_buffer_print(gb);
-
-    gap_buffer_put(gb, '!');                gap_buffer_print(gb);
+    gap_buffer_move_cursor(gb, 3);
+    gap_buffer_put(gb, '!');                    gap_buffer_print(gb);
 
     // this does the same thing as an insert mode put
-    gap_buffer_insert(gb, '!');             gap_buffer_print(gb);
-
-    gap_buffer_put_str(gb, " Bye!");        gap_buffer_print(gb);
-    gap_buffer_put_str(gb, " World!");      gap_buffer_print(gb);
-
-    gap_buffer_move_cursor(gb, -7);         gap_buffer_print(gb);
-
-    gap_buffer_delete(gb);                  gap_buffer_print(gb);
-
-    gap_buffer_move_cursor(gb, DIST_END(gb)); gap_buffer_print(gb);
-
-    gap_buffer_put(gb, '!');             gap_buffer_print(gb);
-
+    gap_buffer_insert(gb, '!');                 gap_buffer_print(gb);
+    gap_buffer_put_str(gb, " Bye!");            gap_buffer_print(gb);
+    gap_buffer_put_str(gb, " World!");          gap_buffer_print(gb);
+    gap_buffer_move_cursor(gb, -7);             gap_buffer_print(gb);
+    gap_buffer_delete(gb);                      gap_buffer_print(gb);
+    gap_buffer_move_cursor(gb, DIST_END(gb));   gap_buffer_print(gb);
+    gap_buffer_put(gb, '!');                    gap_buffer_print(gb);
     gap_buffer_move_cursor(gb, DIST_START(gb));
-
     gap_buffer_move_gap(gb);
-
     gap_buffer_print(gb);
 
     gap_buffer_destroy(gb);
